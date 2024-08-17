464ad72fb620aa4db17957f92e4beb9403dd5f42
Illutas <107239220+Illutas@users.noreply.github.com>
2024-08-17 16:10:19 +0200

    chore: Update TodoItems references to TodoItem in admin, models, and migrations

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,10 @@
 import datetime
 from django.db import models
-from django.conf import settings
-from django.db import models
 from django.contrib.auth.models import User
 
 
 # Create your models here.
-class TodoItems(models.Model):
+class TodoItem(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     created_at = models.DateField(("Date"), default=datetime.date.today)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TodoItem
+from .serializer import TodoSerializer
 
 # Create your views here.
+
+class TodoItemViewSet(viewsets.ModelViewSet):
+
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoSerializer
